3a28a85a2f0987cac105b27ad5ab56b063a2319e
wooBottle <dkrnfls@gmail.com>
2025-08-22 23:50:05 +0900

    Refactor ESLint configuration to use CommonJS syntax and update dependencies in package.json. Remove unused headers configuration from next.config.ts and add skip_lint option in GitHub Actions workflow for deployment.

--- next.config.ts
+++ next.config.ts
@@ -7,27 +7,7 @@ const nextConfig = {
   images: {
     domains: [],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 module.exports = nextConfig;
